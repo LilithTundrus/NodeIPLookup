768c1f279efd8f416d33562677cb5835c1029dcb
Lilith Tundrus <echofox123@gmail.com>
2018-10-07 12:22:55 -0400

    Added some basic information

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,12 @@ import commander from 'commander';
 // Main entry point, call all modules
 
 
+// This script/CLI tool is designed to take an ipv6 or v4 address and retrieve information about it
+// (but not anything special like is it a real IP address/etc.)
+
 // Get the CLI arguments and call the proper module
-commander.version('0.0.1', '-v, --version');
+commander.version('0.0.1', '-v, --version')
+    .option('--v4 <IP Address>', 'Get data from a give IPV4 Address')
+    .option('--v6 <IP Address>', 'Get data from a give IPV6 Address');
 
 commander.parse(process.argv);
-
-
-console.log(commander.args)
-

fa44a0b41fd657c7c40b084928df100f0d09b934
Lilith Tundrus <echofox123@gmail.com>
2018-10-08 19:52:14 -0400

    Now using interfaces and updatng how information is displayed

--- src/lib/lookupv4.ts
+++ src/lib/lookupv4.ts
@@ -1,17 +1,28 @@
 // Using ES6 strict mode (not 100% needed, but ensure that the compiled JS is in strict mode)
 'use strict';
 
-// Node/NPM dependencies
+// Node/NPM and local dependencies
 import request from 'request';
+import { ipInfo } from './interfaces'
 
 // Base URL for IP address lookups
 const baseURL = 'https://ipapi.co/';
+
+
 
 export function v4Lookup(address: string) {
     if (checkIfIPAddressIsValid(address)) {
         request.get(`${baseURL}${address}/json`, (err, res, body) => {
             if (err) {
                 return console.log(`${err}`);
+            }
+
+            if (checkIfJSON(body)) {
+                // Parse the JSON
+                let parsedIPInfo: ipInfo = JSON.parse(body);
+                console.log(parsedIPInfo.ip)
+
+                // For each Property, neatly print it out
             }
 
             // Make sure thhe response is JSON before making it pretty
@@ -24,6 +35,7 @@ export function v4Lookup(address: string) {
     }
 }
 
+// TODO: use isIPv4 function instead
 function checkIfIPAddressIsValid(address: string): boolean {
     if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(address)) {
         return true;

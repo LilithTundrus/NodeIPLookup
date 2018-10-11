066443560c6949fe419407f1b2527ed907c2dc77
Lilith Tundrus <echofox123@gmail.com>
2018-10-11 19:57:25 -0400

    v6 lookup should now work

--- src/lib/lookupv6.ts
+++ src/lib/lookupv6.ts
@@ -4,12 +4,40 @@
 // Node/NPM and local dependencies
 import request from 'request';
 import { ipInfo } from './interfaces'
+import { isIPv6 } from 'net';
 
 // Base URL for IP address lookups
 const baseURL = 'https://ipapi.co/';
 
+// TODO: have this only return the JSON to be cleaned/logged
+export function v6Lookup(address: string) {
+    if (isIPv6(address)) {
+        request.get(`${baseURL}${address}/json`, (err, res, body) => {
+            if (err) {
+                return console.log(`${err}`);
+            }
 
+            // Make sure thhe response is JSON before making it pretty
+            if (checkIfJSON(body)) {
+                // Parse the JSON
+                let parsedIPInfo: ipInfo = JSON.parse(body);
 
+                console.log('\nReturned Information:\n');
+
+                // For each Property, neatly print it out
+                for (let key in parsedIPInfo) {
+                    console.log(`${key}: ${parsedIPInfo[key]}`);
+                }
+            } else {
+                console.log('Something went wrong: Could not parese JSON response from IPV4 lookup');
+            }
+        });
+
+    } else {
+        console.log(`${address} is not a valid IPV4 address`);
+        return process.exit(0);
+    }
+}
 
 function checkIfJSON(jsonString: string): boolean {
     try {

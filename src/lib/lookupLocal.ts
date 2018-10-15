634e4cf6dd14939475c6b55fb0acd305e7aeb96e
Lilith Tundrus <echofox123@gmail.com>
2018-10-14 22:09:49 -0400

    testing local DNS lookup

--- src/lib/lookupLocal.ts
+++ src/lib/lookupLocal.ts
@@ -2,3 +2,11 @@
 'use strict';
 
 // Node/NPM and local dependencies
+
+const dns = require('dns');
+
+export function test() {
+    dns.lookup('192.168.2.23', (err, address, family) => {
+        console.log('address: %j family: IPv%s', address, family);
+    });
+}

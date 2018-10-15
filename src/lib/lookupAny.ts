00348c0fafa33226acb643f6d1862d5c79bd92b8
Lilith Tundrus <echofox123@gmail.com>
2018-10-15 03:51:38 -0400

    Handling errors for lookupAny now

--- src/lib/lookupAny.ts
+++ src/lib/lookupAny.ts
@@ -5,11 +5,11 @@
 
 const dns = require('dns');
 
-
+// This accepts ANY address (Actual IP address or domain name)
 export function lookupAny(address: string) {
     // Use the built-in node.js DNS service to attempt to lookup the address
     dns.resolve(address, function (err, addresses) {
-        if (err) throw err;
+        if (err) return console.log(`Something went wrong during DNS lookup: ${err.message}`);
 
         console.log(`\nAddresses: ${addresses}`);
 
